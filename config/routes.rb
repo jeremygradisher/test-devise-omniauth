2e40f3a42bac0ba8c561d4c182646d61e8d6dc5a
jeremygradisher <jeremygradisher@gmail.com>
2023-05-02 18:06:32 -0400

    Issue with User#edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
   devise_for :users, controllers: {
     registrations: 'user/registrations',
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+  
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
